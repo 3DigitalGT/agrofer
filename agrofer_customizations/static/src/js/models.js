d3bf221b64b138e9aff3ee489c24dd5af9bd386c
Geovany Pérez <desarrollador01@inteligos.gt>
2024-06-27 08:56:43 -0600

    refactor(back-end):CHANGE agrofer_customizations
    Adición de modificaciones al recibo del punto de venta

--- agrofer_customizations/static/src/js/models.js
+++ agrofer_customizations/static/src/js/models.js
@@ -4,18 +4,19 @@ odoo.define("dimona_customizations.models", function (require) {
     var models = require("point_of_sale.models");
     var core = require("web.core");
 
-    models.load_fields("res.company", ["legal_name", "street"]);
+    models.load_fields("res.company", ["legal_name"]);
+    models.load_fields("pos.config", ["pos_address_complete"]);
     let _super_order = models.Order.prototype;
 
     models.Order = models.Order.extend({
         export_for_printing: function () {
             let self = this;
             let company_legal_name = this.pos.company.legal_name || '';
-            let company_street = this.pos.company.street || '';
+            let pos_address_complete = this.pos.config.pos_address_complete || '';
             let receipt = _super_order.export_for_printing.apply(this, arguments);
             receipt = _.extend(receipt, {
                 'company_legal_name': company_legal_name,
-                'company_street': company_street,
+                'pos_address_complete': pos_address_complete,
             });
             return receipt;
         }
